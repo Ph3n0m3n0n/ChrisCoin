9b22b87fa9ce05b205f4300caf3d05e5e371bc2f
Ph3n0m3n0n <kantaris96@my.codergv.org>
2018-01-24 14:15:00 -0600

    updated Genesis Block

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Block {
-    constructor(index, timestamp, data, previousHash = ''){
+    constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
-    }
-
-    calculateHash(){
+    } 
+    calculateHash() {
         return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
     }
 }
 
 class Blockchain {
-    constructor(){
+    constructor() {
         this.chain = [this.generateGenesisBlock()];
     }
-
     generateGenesisBlock() {
-        return new Block (0, "01/02/2018", "This kicks off the ChrisCoin Blockchain", "0");
+        return new Block(0, "02/01/2018", "ChrisCoin to the Moon!", "0");
     }
-
     getLastBlock() {
         return this.chain [this.chain.length -1];
     }
-
     addBlock(newBlock) {
         newBlock.previousHash = this.getLastBlock().hash;
         newBlock.hash = newBlock.calculateHash();
@@ -34,9 +30,9 @@ class Blockchain {
     }
 }
 
-let chrisCoin = new Blockchain();
+let ChrisCoin = new Blockchain();
 
-chrisCoin.addBlock(new Block (1, "01/02/2018", { amount : 100 } ));
-chrisCoin.addBlock(new Block (2, "10/02/2018", { amount : 300 } ));
+ChrisCoin.addBlock(new Block(1, "02/10/2018", {amount: 100}));
+ChrisCoin.addBlock(new Block(2, "02/10/2018", {amount: 300}));
 
-console.log(JSON.stringify(chrisCoin, null, 4));
+console.log(JSON.stringify(ChrisCoin, null, 4));
